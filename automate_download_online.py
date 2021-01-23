05cef6e64e257df81d09c028de767ea9ff540756
Tran Tu Van <trantuvan.kan@gmail.com>
2021-01-23 11:33:18 +0700

    tim hieu them de down data trong ml_2021

---
+++ automate_download_online.py
@@ -0,0 +1,13 @@
+import requests
+import zipfile
+
+r = requests.get(
+    "https://drive.google.com/uc?export=download&id=1QOmVDpd8hcVYqqUXDXf68UMDWQZP0wQV"
+)
+
+# open file in write and btye mode
+with open("data.zip", "wb") as f:
+    f.write(r.content)
+
+with zipfile.ZipFile("data.zip", "r") as data_zip:
+    print(data_zip.namelist())
