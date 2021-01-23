2fbbb613c3558e1e59b2fa671b60cd939d7c434e
Tran Tu Van <trantuvan.kan@gmail.com>
2021-01-23 10:52:01 +0700

    this is the end of compress and und unzip local file

--- automate_scripts.py
+++ automate_scripts.py
@@ -1,15 +1,22 @@
-import zipfile
+# import zipfile
 
-# by defalut not creating a compress file
-# to compress, compression attribute with zipfile.ZIP_DEFLATED is going to help
-with zipfile.ZipFile("files.zip", "w", compression=zipfile.ZIP_DEFLATED) as my_zip:
-    my_zip.write("tidy-data.pdf")
-    my_zip.write("linux.png")
-    my_zip.write("mml-book.pdf")
+# # by defalut not creating a compress file
+# # to compress, compression attribute with zipfile.ZIP_DEFLATED is going to help
+# with zipfile.ZipFile("files.zip", "w", compression=zipfile.ZIP_DEFLATED) as my_zip:
+#     my_zip.write("tidy-data.pdf")
+#     my_zip.write("linux.png")
+#     my_zip.write("mml-book.pdf")
 
-with zipfile.ZipFile("files.zip", "r") as my_zip:
-    # to see the name list in zip
-    print(my_zip.namelist())
-    my_zip.extractall("files")
-    # extract 1 item
-    my_zip.extract("linux.png")
+# with zipfile.ZipFile("files.zip", "r") as my_zip:
+#     # to see the name list in zip
+#     print(my_zip.namelist())
+#     my_zip.extractall("files")
+#     # extract 1 item
+#     my_zip.extract("linux.png")
+
+import shutil
+
+# archive call another, zip files that zipfile extract
+shutil.make_archive("another", "gztar", "files")
+# to extract file
+shutil.unpack_archive("another.zip", "another")
